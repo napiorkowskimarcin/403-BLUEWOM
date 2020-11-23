8ab3a13f1a116c760e5221fb46d6d001955d5482
Marcin Napiorkowski <napiorkowski.marcin.gmail.com>
2020-11-23 11:20:10 +0100

    FETCH CURRENCIES

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const Handlebars = require("handlebars");
 const exphbs = require("express-handlebars");
 const morgan = require("morgan");
-const config = require("./config/config");
 const bodyParser = require("body-parser");
 const path = require("path");
 
 const PORT = process.env.PORT || 3000;
-//load mongoose
-mongoose.connect(config.db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
-//set a DB connection
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  console.log("Connected to the database");
-});
 
 const app = express();
 
--- public/script.js
+++ public/script.js
@@ -1,16 +1,49 @@
 const btnAdd = document.getElementById("btnAdd");
-let currencyName = document.getElementById("currencyName");
+let currencyInput = document.getElementById("currencyName");
 const API_URL = "http://api.nbp.pl/api/exchangerates/tables/a/";
+const API_RATE_URL = "http://api.nbp.pl/api/exchangerates/rates/a/";
 
 let currencyWatched = [];
 
+rmRate = (e) => {
+  e.target.parentNode.remove();
+  console.log("removing");
+};
+
 async function addRate() {
-  console.log("added");
-  console.log(currencyName.value);
-  let currencyArray = await fetch(API_URL);
+  console.log(API_RATE_URL + currencyInput.value);
+
+  let currencyArray = await fetch(API_RATE_URL + currencyInput.value);
   currencyArray = await currencyArray.json();
-  currencyArray = currencyArray[0].rates;
-  console.log(currencyArray);
+
+  let appendName = currencyArray.currency;
+  let appendCode = currencyArray.code;
+  let appendRate = currencyArray.rates[0].mid;
+
+  console.log({ appendName });
+  console.log({ appendCode });
+  console.log({ appendRate });
+
+  let trAppend = document.createElement("tr");
+  let tdAppendName = document.createElement("td");
+  tdAppendName.textContent = appendName;
+  let tdAppendCode = document.createElement("td");
+  tdAppendCode.textContent = appendCode;
+  let tdAppendRate = document.createElement("td");
+  tdAppendRate.textContent = appendRate;
+  let tdBtnRemove = document.createElement("td");
+  let btnRemove = document.createElement("button");
+  btnRemove.textContent = "REMOVE";
+  btnRemove.classList.add("removeCurrency", "btn", "btn-primary");
+
+  currencyInput = "";
+
+  trAppend.appendChild(tdAppendName);
+  trAppend.appendChild(tdAppendCode);
+  trAppend.appendChild(tdAppendRate);
+  tdBtnRemove.appendChild(btnRemove);
+  trAppend.appendChild(tdBtnRemove);
+  document.getElementById("table").appendChild(trAppend);
 }
 
 btnAdd.addEventListener("click", addRate);
